bd48929328e1160889ace92a092cd10dc2fea319
Shuvayu <shuvayu.dhar@4mation.com.au>
2017-07-07 16:08:37 +1000

    more commits

--- src/MFA/Controllers/ImageController.cs
+++ src/MFA/Controllers/ImageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace MFA.Controllers
 {
@@ -12,6 +13,13 @@ namespace MFA.Controllers
         public IActionResult Index()
         {
             ViewData["Message"] = "Upload Image";
+
+            return View();
+        }
+
+        public IActionResult Imageupload(IFormFile image)
+        {
+            ViewData["Message"] = "Upload Image Uploaded";
 
             return View();
         }
